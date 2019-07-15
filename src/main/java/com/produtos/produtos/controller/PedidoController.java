8e0bc3292b362645115b54f2c5527f1e942ae357
MarcusMaziero <marcus.maziero@outlook.com>
2019-07-15 20:08:02 -0300

    Altera PedidoController para chamar class PedidoBusiness

--- src/main/java/com/produtos/produtos/controller/PedidoController.java
+++ src/main/java/com/produtos/produtos/controller/PedidoController.java
@@ -6,19 +6,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.produtos.produtos.business.PedidoBusiness;
 import com.produtos.produtos.models.Pedido;
-import com.produtos.produtos.repository.PedidoRepository;
 
+/**
+ * Classe onde é tratada as rotas do sistema referente aos Pedidos
+ * 
+ * @author Marcus Vinicius
+ *
+ */
 @RestController
 @RequestMapping("/api")
 public class PedidoController {
 
 	@Autowired
-	private PedidoRepository repository;
-
+	private PedidoBusiness business;
+	
 	@PostMapping("/v1/pedido")
-	public Pedido criarPedido(@RequestBody Pedido pedido) {
-		return repository.save(pedido);
+	public Object criarPedido(@RequestBody Pedido pedido) {
+		return this.business.persiste(pedido);
 	}
 
 }
