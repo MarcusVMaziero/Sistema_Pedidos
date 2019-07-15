f3480397af615869be77b76566a4332d4d12ee1a
MarcusMaziero <marcus.maziero@outlook.com>
2019-07-15 19:14:03 -0300

    Altera PedidoController para novo modelo

--- src/main/java/com/produtos/produtos/controller/PedidoController.java
+++ src/main/java/com/produtos/produtos/controller/PedidoController.java
@@ -1,5 +1,24 @@
 package com.produtos.produtos.controller;
 
-public class pedido {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.produtos.models.Pedido;
+import com.produtos.produtos.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/api")
+public class PedidoController {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	@PostMapping("/v1/pedido")
+	public Pedido criarPedido(@RequestBody Pedido pedido) {
+		return repository.save(pedido);
+	}
 
 }
