33c5bcd64b0a9172c3c5de87e50d182edc961e9a
MarcusMaziero <marcus.maziero@outlook.com>
2019-07-15 21:26:34 -0300

    Adiciona teste de save repository

---
+++ src/test/java/com/produtos/produtos/repository/PedidoRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.produtos.produtos.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.produtos.produtos.models.Pedido;
+
+/**
+ * Classe de teste para validar se esta persistindo na base de Dados
+ * 
+ * @author Marcus Vinicius
+ *
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PedidoRepositoryTest {
+
+	@Autowired
+	private PedidoRepository repository;
+
+	@Test
+	public void persistePedido() {
+		Pedido pedido = mockPedido();
+
+		repository.save(pedido);
+
+		Assertions.assertThat(pedido.getId()).isNotNull();
+		Assertions.assertThat(pedido.getCodCliente()).isEqualTo(1L);
+		Assertions.assertThat(pedido.getCodProdutos()).isEqualTo(1L);
+		Assertions.assertThat(pedido.getEnderecoEntrega()).isEqualTo("teste");
+		Assertions.assertThat(pedido.getValorTotal()).isEqualTo(100D);
+	}
+
+	private Pedido mockPedido() {
+		Pedido p = new Pedido();
+
+		p.setCodCliente(1L);
+		p.setCodProdutos(1L);
+		p.setEnderecoEntrega("teste");
+		p.setValorTotal(100D);
+
+		return p;
+	}
+}
