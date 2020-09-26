1cfc76a25fd137235a0e0a892e8df8bc9553bac5
MarcusMaziero <marcus.maziero@outlook.com>
2020-09-25 21:41:55 -0300

    [maziero] Cria Dataprovider de pedidos

---
+++ src/main/java/com/pedidos/pedidos/dataprovider/pedidos/OrderDataProvider.java
@@ -0,0 +1,27 @@
+package com.pedidos.pedidos.dataprovider.pedidos;
+
+import com.pedidos.pedidos.core.Order;
+import com.pedidos.pedidos.dataprovider.pedidos.gateway.OrderGateway;
+import com.pedidos.pedidos.dataprovider.pedidos.models.OrderData;
+import com.pedidos.pedidos.dataprovider.pedidos.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class OrderDataProvider implements OrderGateway {
+    private OrderRepository repository;
+
+    @Override
+    public Order create(Order order) {
+        return repository.save(order.toOrderData()).toDomain();
+    }
+
+    @Override
+    public List<Order> findOrders() {
+        return repository.findAll().stream().map(OrderData::toDomain).collect(Collectors.toList());
+    }
+}
