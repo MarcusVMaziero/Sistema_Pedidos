ef248f0a25701db9ed3ce06a2e4d90c25e13c6dd
MarcusMaziero <marcus.maziero@outlook.com>
2020-09-30 00:34:34 -0300

    [maziero] Adiciona send para rabbitmq

--- src/main/java/com/pedidos/pedidos/dataprovider/pedidos/OrderDataProvider.java
+++ src/main/java/com/pedidos/pedidos/dataprovider/pedidos/OrderDataProvider.java
@@ -1,22 +1,30 @@
 package com.pedidos.pedidos.dataprovider.pedidos;
 
 import com.pedidos.pedidos.core.Order;
+import com.pedidos.pedidos.dataprovider.entregaqueue.gateway.DeliveryQueueGateway;
+import com.pedidos.pedidos.dataprovider.entregaqueue.models.DeliveryQueue;
 import com.pedidos.pedidos.dataprovider.pedidos.gateway.OrderGateway;
 import com.pedidos.pedidos.dataprovider.pedidos.models.OrderData;
 import com.pedidos.pedidos.dataprovider.pedidos.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class OrderDataProvider implements OrderGateway {
+
     private final OrderRepository repository;
+    private final DeliveryQueueGateway deliveryQueueGateway;
 
     @Override
     public Order create(Order order) {
+        deliveryQueueGateway.send(DeliveryQueue.builder()
+                .idOrder(order.getIdOrder())
+                .deliveryAddress(order.getDeliveryAddress())
+                .build());
         return repository.save(order.toOrderData()).toDomain();
     }
 
