866b399040b7e5e56129b2d474fcd90e07b75cac
MarcusMaziero <marcus.maziero@outlook.com>
2020-09-30 00:35:16 -0300

    [maziero] Adiciona configs do rabbitmq

--- src/main/java/com/pedidos/pedidos/PedidosApplication.java
+++ src/main/java/com/pedidos/pedidos/PedidosApplication.java
@@ -1,12 +1,24 @@
 package com.pedidos.pedidos;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class PedidosApplication {
 
+	@Value("${queue.order.name}")
+	private String orderQueue;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+	@Bean
+	public Queue queue() {
+		return new Queue(orderQueue, true);
 	}
 }
